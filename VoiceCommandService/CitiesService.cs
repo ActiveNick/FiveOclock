0f75950d3d88ab45566f13953f2a1c91d4b91ee7
Nick Landry <nlandry@activenick.net>
2016-07-23 09:37:57 -0400

    Initial commit from local folder

---
+++ VoiceCommandService/CitiesService.cs
@@ -0,0 +1,234 @@
+﻿//==========================================================================
+//
+// Author:  Nick Landry
+// Title:   Senior Technical Evangelist - Microsoft US DX - NY Metro
+// Twitter: @ActiveNick
+// Blog:    www.AgeofMobility.com
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// Disclaimer: Portions of this code may been simplified to demonstrate
+// useful application development techniques and enhance readability.
+// As such they may not necessarily reflect best practices in enterprise 
+// development, and/or may not include all required safeguards.
+// 
+// This code and information are provided "as is" without warranty of any
+// kind, either expressed or implied, including but not limited to the
+// implied warranties of merchantability and/or fitness for a particular
+// purpose.
+//==========================================================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveOclock.VoiceCommands
+{
+    // A service that analyzes time zones and returns a list of sample cities
+    // where it's after 5:00PM
+    public sealed class CitiesService
+    {
+        public IList<City> GetCities()
+        {
+            // Fetch the time zone where it's currently 5:00PM
+            int DesiredTimeZone = GetDesiredTimezone();
+
+            List<City> resultCities = new List<City>();
+
+            switch (DesiredTimeZone)
+            {
+                case 14:
+                    // There's not a lot smack in the middle of the Pacific :)
+                    resultCities.Add(new City("Line Islands", "Kiribati"));
+                    break;
+                case 13:
+                    resultCities.Add(new City("Phoenix Islands", "Kiribati"));
+                    resultCities.Add(new City("Nuku'alofa", "Tonga"));
+                    resultCities.Add(new City("Fakaofo", "Tokelau"));
+                    break;
+                case 12:
+                    resultCities.Add(new City("Suva", "Fiji"));
+                    resultCities.Add(new City("Kamchatka Krai", "Russia"));
+                    resultCities.Add(new City("Gilbert Islands", "Kiribati"));
+                    break;
+                case 11:
+                    resultCities.Add(new City("Nouméa", "New Caledonia"));
+                    resultCities.Add(new City("Primorsky Krai", "Russia"));
+                    resultCities.Add(new City("Honiara", "Solomon Islands"));
+                    break;
+                case 10:
+                    resultCities.Add(new City("Brisbane", "Australia"));
+                    resultCities.Add(new City("Hagåtña", "Guam"));
+                    resultCities.Add(new City("Port Moresby", "Papua New Guinea"));
+                    break;
+                case 9:
+                    resultCities.Add(new City("Tokyo", "Japan"));
+                    resultCities.Add(new City("Seoul", "South Korea"));
+                    resultCities.Add(new City("Jakarta", "Indonesia"));
+                    break;
+                case 8:
+                    resultCities.Add(new City("Hong Kong", "China"));
+                    resultCities.Add(new City("Kuala Lumpur", "Malaysia"));
+                    resultCities.Add(new City("Manila", "Philippines"));
+                    break;
+                case 7:
+                    resultCities.Add(new City("Bangkok", "Thailand"));
+                    resultCities.Add(new City("Hanoi", "Vietnam"));
+                    resultCities.Add(new City("Phnom Penh", "Cambodia"));
+                    break;
+                case 6:
+                    resultCities.Add(new City("Dhaka", "Bangladesh"));
+                    resultCities.Add(new City("Astana", "Kazakhstan"));
+                    resultCities.Add(new City("Thimphu", "Bhutan"));
+                    break;
+                case 5:
+                    resultCities.Add(new City("New Delhi", "India"));
+                    resultCities.Add(new City("Colombo", "Sri Lanka"));
+                    resultCities.Add(new City("Islamabad", "Pakistan"));
+                    break;
+                case 4:
+                    resultCities.Add(new City("Dubai", "United Arab Emirates"));
+                    resultCities.Add(new City("Tbilisi", "Georgia"));
+                    resultCities.Add(new City("Yerevan", "Armenia"));
+                    break;
+                case 3:
+                    resultCities.Add(new City("Moscow", "Russia"));
+                    resultCities.Add(new City("Minsk", "Belarus"));
+                    resultCities.Add(new City("Kampala", "Uganda"));
+                    break;
+                case 2:
+                    resultCities.Add(new City("Athens", "Greece"));
+                    resultCities.Add(new City("Cairo", "Egypt"));
+                    resultCities.Add(new City("Tunis, Tunisia", ""));
+                    resultCities.Add(new City("Helsinki", "Finland"));
+                    break;
+                case 1:
+                    resultCities.Add(new City("Paris", "France"));
+                    resultCities.Add(new City("Brussels", "Belgium"));
+                    resultCities.Add(new City("Berlin", "Germany"));
+                    resultCities.Add(new City("Rome", "Italy"));
+                    resultCities.Add(new City("Prague", "Czech Republic"));
+                    resultCities.Add(new City("Madrid", "Spain"));
+                    resultCities.Add(new City("Amsterdam", "Netherlands"));
+                    break;
+                case 0:
+                    resultCities.Add(new City("London", "United Kingdom"));
+                    resultCities.Add(new City("Dublin", "Ireland"));
+                    resultCities.Add(new City("Reykjavík", "Iceland"));
+                    break;
+                case -1:
+                    resultCities.Add(new City("Praia", "Cape Verde"));
+                    resultCities.Add(new City("Ponta Delgada", "Azores"));
+                    break;
+                case -2:
+                    resultCities.Add(new City("Fernando de Noronha", "Brazil"));
+                    resultCities.Add(new City("King Edward Point", "South Georgia and the South Sandwich Islands"));
+                    break;
+                case -3:
+                    resultCities.Add(new City("Rio de Janeiro", "Brazil"));
+                    resultCities.Add(new City("Montevideo", "Uruguay"));
+                    resultCities.Add(new City("Buenos Aires", "Argentina"));
+                    break;
+                case -4:
+                    resultCities.Add(new City("Halifax", "Canada"));
+                    resultCities.Add(new City("San Juan", "Puerto Rico"));
+                    resultCities.Add(new City("Bridgetown", "Barbados"));
+                    break;
+                case -5:
+                    // Home time zone of the developer :)
+                    resultCities.Add(new City("New York City", "United States"));
+                    resultCities.Add(new City("Montreal", "Canada"));
+                    resultCities.Add(new City("Kingston", "Jamaica"));
+                    resultCities.Add(new City("Nassau", "Bahamas"));
+                    resultCities.Add(new City("Bogotá", "Columbia"));
+                    resultCities.Add(new City("Lima", "Peru"));
+                    break;
+                case -6:
+                    resultCities.Add(new City("Chicago", "United States"));
+                    resultCities.Add(new City("Winnipeg", "Canada"));
+                    resultCities.Add(new City("Mexico City", "Mexico"));
+                    resultCities.Add(new City("San Salvador", "El Salvador"));
+                    resultCities.Add(new City("Guatemala City", "Guatemala"));
+                    resultCities.Add(new City("Tegucigalpa", "Honduras"));
+                    break;
+                case -7:
+                    resultCities.Add(new City("Calgary", "Canada"));
+                    resultCities.Add(new City("Denver", "United States"));
+                    resultCities.Add(new City("Chihuahua", "Mexico"));
+                    break;
+                case -8:
+                    resultCities.Add(new City("Vancouver", "Canada"));
+                    resultCities.Add(new City("Seattle", "United States"));
+                    resultCities.Add(new City("San Francisco", "United States"));
+                    resultCities.Add(new City("Tijuana", "Mexico"));
+                    break;
+                case -9:
+                    resultCities.Add(new City("Anchorage", "United States"));
+                    resultCities.Add(new City("Gambier Islands", "French Polynesia"));
+                    break;
+                case -10:
+                    resultCities.Add(new City("Honolulu", "United States"));
+                    resultCities.Add(new City("Pape'ete", "French Polynesia"));
+                    break;
+                case -11:
+                    resultCities.Add(new City("Pago Pago", "American Samoa"));
+                    resultCities.Add(new City("Alofi", "Niue"));
+                    break;
+                default:
+                    break;
+            }
+
+            return resultCities;
+        }
+
+        // Calculates the desired time zone where it's currently 5:00PM
+        private static int GetDesiredTimezone()
+        {
+            // Capturing the current time and Utc time to avoid rare cases
+            // where the hour might change mid-routine
+            DateTime currenttime = DateTime.Now;
+            DateTime currentUtctime = DateTime.UtcNow;
+
+            // We don't care about minutes so we just work with hours rounded down
+            int currenthour = currenttime.Hour;
+            int currentUtcHour = currentUtctime.Hour;
+            // If the day isn't the same for the locale vs Utc, we have to 
+            // compensate by adding 24 hours to the Utc time
+            if (currenttime.Day != currentUtctime.Day) { currentUtcHour += 24; }
+
+            int UtcDelta = (currenthour - currentUtcHour);
+            // We need to artificially compensate for daylight savings
+            if (currenttime.IsDaylightSavingTime()) { UtcDelta -= 1; }
+            int DeltaToFive = (17 - currenthour);
+            int DesiredTimeZone = (UtcDelta + DeltaToFive);
+            // If we go over UTC+14, we need to go back a day by 25h
+            if (DesiredTimeZone > 14) { DesiredTimeZone -= 25; }
+            return DesiredTimeZone;
+        }
+    }
+
+    // Simple data class to hold city/country info for the list
+    public sealed class City
+    {
+        public City(string n, string c)
+        {
+            _name = n;
+            _country = c;
+        }
+
+        private string _name;
+        private string _country;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Country
+        {
+            get { return _country; }
+            set { _country = value; }
+        }
+    }
+}
---
+++ VoiceCommandService/VoiceCommandService.cs
@@ -0,0 +1,250 @@
+﻿//==========================================================================
+//
+// Author:  Nick Landry
+// Title:   Senior Technical Evangelist - Microsoft US DX - NY Metro
+// Twitter: @ActiveNick
+// Blog:    www.AgeofMobility.com
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// Disclaimer: Portions of this code may been simplified to demonstrate
+// useful application development techniques and enhance readability.
+// As such they may not necessarily reflect best practices in enterprise 
+// development, and/or may not include all required safeguards.
+// 
+// This code and information are provided "as is" without warranty of any
+// kind, either expressed or implied, including but not limited to the
+// implied warranties of merchantability and/or fitness for a particular
+// purpose.
+//==========================================================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.VoiceCommands;
+
+namespace FiveOclock.VoiceCommands
+{
+    public sealed class FOCVoiceCommandService : IBackgroundTask
+    {
+        /// <summary>
+        /// the service connection is maintained for the lifetime of a cortana session, once a voice command
+        /// has been triggered via Cortana.
+        /// </summary>
+        VoiceCommandServiceConnection voiceServiceConnection;
+
+        /// <summary>
+        /// Lifetime of the background service is controlled via the BackgroundTaskDeferral object, including
+        /// registering for cancellation events, signalling end of execution, etc. Cortana may terminate the 
+        /// background service task if it loses focus, or the background task takes too long to provide.
+        /// 
+        /// Background tasks can run for a maximum of 30 seconds.
+        /// </summary>
+        BackgroundTaskDeferral serviceDeferral;
+
+        /// <summary>
+        /// Background task entrypoint. Voice Commands using the <VoiceCommandService Target="...">
+        /// tag will invoke this when they are recognized by Cortana, passing along details of the 
+        /// invocation. 
+        /// 
+        /// Background tasks must respond to activation by Cortana within 0.5 seconds, and must 
+        /// report progress to Cortana every 5 seconds (unless Cortana is waiting for user
+        /// input). There is no execution time limit on the background task managed by Cortana,
+        /// but developers should use plmdebug (https://msdn.microsoft.com/en-us/library/windows/hardware/jj680085%28v=vs.85%29.aspx)
+        /// on the Cortana app package in order to prevent Cortana timing out the task during
+        /// debugging.
+        /// 
+        /// Cortana dismisses its UI if it loses focus. This will cause it to terminate the background
+        /// task, even if the background task is being debugged. Use of Remote Debugging is recommended
+        /// in order to debug background task behaviors. In order to debug background tasks, open the
+        /// project properties for the app package (not the background task project), and enable
+        /// Debug -> "Do not launch, but debug my code when it starts". Alternatively, add a long
+        /// initial progress screen, and attach to the background task process while it executes.
+        /// </summary>
+        /// <param name="taskInstance">Connection to the hosting background service process.</param>
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            serviceDeferral = taskInstance.GetDeferral();
+
+            // Register to receive an event if Cortana dismisses the background task. This will
+            // occur if the task takes too long to respond, or if Cortana's UI is dismissed.
+            // Any pending operations should be cancelled or waited on to clean up where possible.
+            taskInstance.Canceled += OnTaskCanceled;
+
+            var triggerDetails = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+
+            // This should match the uap:AppService and VoiceCommandService references from the 
+            // package manifest and VCD files, respectively. Make sure we've been launched by
+            // a Cortana Voice Command.
+            if (triggerDetails != null && triggerDetails.Name == "FOCVoiceCommandService")
+            {
+                try
+                {
+                    voiceServiceConnection =
+                        VoiceCommandServiceConnection.FromAppServiceTriggerDetails(
+                            triggerDetails);
+
+                    voiceServiceConnection.VoiceCommandCompleted += OnVoiceCommandCompleted;
+
+                    Windows.ApplicationModel.VoiceCommands.VoiceCommand voiceCommand = 
+                        await voiceServiceConnection.GetVoiceCommandAsync();
+                    
+                    // Depending on the operation (defined in AdventureWorks:AdventureWorksCommands.xml)
+                    // perform the appropriate command.
+                    switch (voiceCommand.CommandName)
+                    {
+                        case "FiveOclockWhere":
+                            //var destination = voiceCommand.Properties["destination"][0];
+                            await FindCityWhereICanDrink();
+                            break;
+
+                        default:
+                            // As with app activation VCDs, we need to handle the possibility that
+                            // an app update may remove a voice command that is still registered.
+                            // This can happen if the user hasn't run an app since an update.
+                            LaunchAppInForeground();
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine("Handling Voice Command failed " + ex.ToString());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Show a progress screen. These should be posted at least every 5 seconds for a 
+        /// long-running operation, such as accessing network resources over a mobile 
+        /// carrier network.
+        /// </summary>
+        /// <param name="message">The message to display, relating to the task being performed.</param>
+        /// <returns></returns>
+        private async Task ShowProgressScreen(string message)
+        {
+            var userProgressMessage = new VoiceCommandUserMessage();
+            userProgressMessage.DisplayMessage = userProgressMessage.SpokenMessage = message;
+
+            VoiceCommandResponse response = VoiceCommandResponse.CreateResponse(userProgressMessage);
+            await voiceServiceConnection.ReportProgressAsync(response);
+        }
+
+        /// <summary>
+        /// Find a city in the world where it's at least 5 o'clock right now
+        /// </summary>
+        /// <param></param>
+        /// <returns></returns>
+        private async Task FindCityWhereICanDrink()
+        {
+            // If this operation is expected to take longer than 0.5 seconds, the task must
+            // provide a progress response to Cortana prior to starting the operation, and
+            // provide updates at most every 5 seconds.
+            await ShowProgressScreen("Looking for drinking destinations around the globe...");
+
+            var userMessage = new VoiceCommandUserMessage();
+            var locationsContentTiles = new List<VoiceCommandContentTile>();
+            // Set a title message for the page.
+            string message = "";
+
+            // Let's determine what time it is, and if we're in the "drinking zone"
+            DateTime currenttime = DateTime.Now;
+            int currenthour = currenttime.Hour;
+
+            if (currenthour < 3 || currenthour >= 17)
+            {
+                message = "It's past 5 o'clock already, time to raise your glass. Cheers!";
+            }
+            else if (currenthour >= 3 && currenthour < 7)
+            {
+                message = "It's getting late and you probably had enough. Time to hit the sack...";
+            }
+            else
+            {
+                // Get the list of cities where it's currently 5:00PM
+                CitiesService cs = new CitiesService();
+                List<City> citylist = cs.GetCities() as List<City>;
+
+                if (citylist.Count > 0)
+                {
+                    message = "Here are cities where people are happily drinking now:";
+
+                    // fill in tiles for each drinking location, to display information about cities without
+                    // launching the app.
+                    foreach (City city in citylist)
+                    {
+                        var locationTile = new VoiceCommandContentTile();
+
+                        locationTile.ContentTileType = VoiceCommandContentTileType.TitleWith68x68IconAndText;
+                        locationTile.Image = await Package.Current.InstalledLocation.GetFileAsync("FiveOclock.VoiceCommands\\Images\\Beverage-Alcohol.png");
+
+                        locationTile.Title = city.Name;
+                        locationTile.TextLine1 = city.Country;
+
+                        locationsContentTiles.Add(locationTile);
+                    } 
+                } else
+                {
+                    message = "Something went wrong. Oh well, just have that drink anyways, I won't tell.";
+                }
+            }
+            userMessage.DisplayMessage = message;
+            userMessage.SpokenMessage = message;
+            var response = VoiceCommandResponse.CreateResponse(userMessage, locationsContentTiles);
+
+            await voiceServiceConnection.ReportSuccessAsync(response);
+        }
+
+        /// <summary>
+        /// Provide a simple response that launches the app. Expected to be used in the
+        /// case where the voice command could not be recognized (eg, a VCD/code mismatch.)
+        /// </summary>
+        private async void LaunchAppInForeground()
+        {
+            var userMessage = new VoiceCommandUserMessage();
+            userMessage.SpokenMessage = "Launching Five O'clock";
+
+            var response = VoiceCommandResponse.CreateResponse(userMessage);
+
+            response.AppLaunchArgument = "";
+
+            await voiceServiceConnection.RequestAppLaunchAsync(response);
+        }
+
+        /// <summary>
+        /// Handle the completion of the voice command. Your app may be cancelled
+        /// for a variety of reasons, such as user cancellation or not providing 
+        /// progress to Cortana in a timely fashion. Clean up any pending long-running
+        /// operations (eg, network requests).
+        /// </summary>
+        /// <param name="sender">The voice connection associated with the command.</param>
+        /// <param name="args">Contains an Enumeration indicating why the command was terminated.</param>
+        private void OnVoiceCommandCompleted(VoiceCommandServiceConnection sender, VoiceCommandCompletedEventArgs args)
+        {
+            if (this.serviceDeferral != null)
+            {
+                this.serviceDeferral.Complete();
+            }
+        }
+
+        /// <summary>
+        /// When the background task is cancelled, clean up/cancel any ongoing long-running operations.
+        /// This cancellation notice may not be due to Cortana directly. The voice command connection will
+        /// typically already be destroyed by this point and should not be expected to be active.
+        /// </summary>
+        /// <param name="sender">This background task instance</param>
+        /// <param name="reason">Contains an enumeration with the reason for task cancellation</param>
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            System.Diagnostics.Debug.WriteLine("Task cancelled, clean up");
+            if (this.serviceDeferral != null)
+            {
+                //Complete the service deferral
+                this.serviceDeferral.Complete();
+            }
+        }
+    }
+}
